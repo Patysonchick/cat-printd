c1c628ab5c014caa1608bec5d006b66db621a4bc
Эдгар Томский <edgarkozlov3229@gmail.com>
2025-12-07 13:37:15 +0300

    Printing images

--- src/printer.rs
+++ src/printer.rs
@@ -2,16 +2,20 @@ use btleplug::{
     api::{Central, Manager as _, Peripheral as _, ScanFilter, WriteType},
     platform::{Manager, Peripheral},
 };
+use image::DynamicImage;
 use std::time::Duration;
 use tokio::time;
 use uuid::Uuid;
 
 const YHK_SERVICE_UUID: &str = "49535343-fe7d-4ae5-8fa9-9fafd205e455";
 const YHK_WRITE_CHAR_UUID: &str = "49535343-8841-43f4-a8d4-ecbe34729bb3";
+const YHK_WIDTH: u16 = 384;
+const YHK_BYTES: u8 = (384 / 8) as u8; // 48 bytes
 
 pub struct Printer {
     model: Models,
-    pub peripheral: Peripheral, // TODO! вернуть в приватный
+    width: u16,
+    peripheral: Peripheral,
 }
 
 impl Printer {
@@ -46,7 +50,11 @@ impl Printer {
         println!("Connecting to {}...", peripheral.address());
         peripheral.connect().await?;
 
-        let printer = Printer { model, peripheral };
+        let printer = Printer {
+            model,
+            peripheral,
+            width: YHK_WIDTH,
+        };
         Ok(printer)
     }
 
@@ -66,9 +74,8 @@ impl Printer {
             .expect("Characteristic not found");
 
         println!("Init...");
-        let cmd_init = [0x1b, 0x40];
         self.peripheral
-            .write(write_char, &cmd_init, WriteType::WithoutResponse)
+            .write(write_char, &[0x1b, 0x40], WriteType::WithoutResponse)
             .await?;
         time::sleep(Duration::from_millis(500)).await;
 
@@ -83,9 +90,12 @@ impl Printer {
             .find(|c| c.uuid == Uuid::parse_str(YHK_WRITE_CHAR_UUID).unwrap())
             .expect("Characteristic not found");
 
-        let cmd_start = [0x1d, 0x49, 0xf0, 0x19];
         self.peripheral
-            .write(write_char, &cmd_start, WriteType::WithoutResponse)
+            .write(
+                write_char,
+                &[0x1d, 0x49, 0xf0, 0x19],
+                WriteType::WithoutResponse,
+            )
             .await?;
         time::sleep(Duration::from_millis(500)).await;
 
@@ -100,12 +110,89 @@ impl Printer {
             .find(|c| c.uuid == Uuid::parse_str(YHK_WRITE_CHAR_UUID).unwrap())
             .expect("Characteristic not found");
 
-        let cmd_end = vec![0x0a, 0x0a, 0x0a, 0x0a];
         self.peripheral
-            .write(write_char, &cmd_end, WriteType::WithoutResponse)
+            .write(
+                write_char,
+                &[0x0a, 0x0a, 0x0a, 0x0a],
+                WriteType::WithoutResponse,
+            )
             .await?;
         time::sleep(Duration::from_secs(1)).await;
 
+        Ok(())
+    }
+
+    pub async fn print_line(&self, line_data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+        // Если печатает негатив, замените 0xFF на 0x00
+        // let byte_val: u8 = if i % 2 == 0 { 0xFF } else { 0x00 };
+        // let line_data = [byte_val; YHK_WIDTH_BYTES];
+
+        // Формат: [GS v 0] [m] [xL] [xH] [yL] [yH] [DATA]
+        let mut packet: [u8; 8 + YHK_BYTES as usize] = [0; 8 + YHK_BYTES as usize];
+
+        packet[0] = 0x1d;
+        packet[1] = 0x76;
+        packet[2] = 0x30;
+        packet[3] = 0x00;
+        packet[4] = YHK_BYTES; // xL (48 байт)
+        packet[5] = 0x00; // xH
+        packet[6] = 0x01; // yL (Высота = 1 строка)
+        packet[7] = 0x00; // yH
+        for i in 0..YHK_BYTES {
+            packet[(i + 8) as usize] = line_data[i as usize];
+        }
+
+        let chars = self.peripheral.characteristics();
+        let write_char = chars
+            .iter()
+            .find(|c| c.uuid == Uuid::parse_str(YHK_WRITE_CHAR_UUID).unwrap())
+            .expect("Characteristic not found");
+
+        // for chunk in packet.chunks(20) {
+        //     self.peripheral
+        //         .write(write_char, chunk, WriteType::WithoutResponse)
+        //         .await?;
+        // }
+        self.peripheral
+            .write(write_char, &packet, WriteType::WithoutResponse)
+            .await?;
+
+        time::sleep(Duration::from_millis(20)).await;
+
+        Ok(())
+    }
+
+    pub async fn print_image(&self, img: DynamicImage) -> Result<(), Box<dyn std::error::Error>> {
+        let img = img.resize(
+            YHK_WIDTH as u32,
+            YHK_WIDTH as u32,
+            image::imageops::FilterType::Nearest,
+        );
+        let img_gray = img.to_luma8();
+
+        println!("Printing...");
+        self.start_print_sequence().await?;
+
+        for y in 0..YHK_WIDTH {
+            let mut line_data = [0u8; YHK_BYTES as usize];
+
+            for x in 0..YHK_WIDTH {
+                let pixel = img_gray.get_pixel(x as u32, y as u32);
+                let brightness = pixel[0]; // 0..255
+
+                // Если картинка инвертирована, поменяй знак (< на >).
+                if brightness < 128 {
+                    let byte_idx = (x / 8) as usize;
+                    let bit_idx = 7 - (x % 8); // Старший бит слева
+                    line_data[byte_idx] |= 1 << bit_idx;
+                }
+            }
+
+            self.print_line(&line_data).await?;
+        }
+
+        self.stop_print_sequence().await?;
+        println!("Done!");
         Ok(())
     }
 }
